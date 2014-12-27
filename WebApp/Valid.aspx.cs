a2f4d4bb26f763b7145f578d24c45708285a06c5
denggaoshan <475665610@qq.com>
2014-12-28 00:50:06 +0800

    to be finished

--- WebApp/Valid.aspx.cs
+++ WebApp/Valid.aspx.cs
@@ -9,13 +9,16 @@ namespace WebApp
 {
     public partial class MainWeb : System.Web.UI.Page
     {
+        private int UserID;
         protected void Page_Load(object sender, EventArgs e)
         {
             var name = Request.QueryString["user"];
             var psw = Request.QueryString["psw"];
-            
+
+            UserID = CheckUser(name, psw);
+
             //验证用户名密码：
-            if (name == "denggaoshan" && psw == "5610201")
+            if (UserID != null )
             {
                 Response.Redirect("./Person.aspx", false);
             }
@@ -23,6 +26,20 @@ namespace WebApp
             {
                 Response.Redirect("ValidError.aspx", false); 
             }
+        }
+
+        private int CheckUser(string name,string psw)
+        {
+            DataLinkDataContext ctx = new DataLinkDataContext();
+
+            var query = from user in ctx.User
+                where user.username == "denggaoshan" && user.password == "5610201"
+                select user;
+
+            var cont = query.Count();
+
+            ctx.SubmitChanges();
+            return 1;
         }
     }
 }
